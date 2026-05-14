5e19eb51d1774d8d8b8137e6e7d14ca093746560
Abhishek S L <abhisheksl081@gmail.com>
2026-05-14 11:18:42 +0530

    Update GlobalFallBackController.java

--- src/main/java/com/spring/controller/GlobalFallBackController.java
+++ src/main/java/com/spring/controller/GlobalFallBackController.java
@@ -1,14 +1,12 @@
 package com.spring.controller;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GlobalFallBackController implements ErrorController {
-
     @RequestMapping("/error")
     public String handleError() {
-        return "redirect:/restaurant";
+        return "error";
     }
 }
